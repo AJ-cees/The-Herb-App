d56b97b303515db10c4c49815263cff4617ca731
AJ-cees <aj.observer09@gmail.com>
2022-11-30 14:59:43 -0500

    Herb List Addition
    A list of herbs has been created. 4 herbs and their properties were added to the list.

--- herb-js-/js/scripts.js
+++ herb-js-/js/scripts.js
@@ -1,3 +1,40 @@
-alert('Hello World');
-let favoriteFood = 'Tiramasu';
-document.write(favoriteFood);
+
+let herbList = [
+    {
+        name: 'lavender',
+        latin: 'Lavandula spp',
+        type: 'antibacterial',
+        color: 'purple',
+        height: 20
+    }
+    {
+        name: 'rosemary',
+        latin: 'Rosmarinus officinalis',
+        type: 'antimicrobial',
+        color: 'green',
+        height: 6
+    }
+
+    {
+        name:  'peppermint',
+        latin:  'Mentha x piperita',
+        types:  'antimicrobial' , 
+        color:  'green',
+        height:  18
+    
+ }
+   {
+    name: 'chamomile',
+    latin: 'Matricaria chamomilla',
+    type: 'calmative',
+    color: 'white',
+    height: 3
+}
+];
+ 
+
+
+
+ 
+
+ 
