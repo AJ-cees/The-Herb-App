ad7cc68d1eb75e0eb950eb46cb4eb22a496fb094
AJ-cees <aj.observer09@gmail.com>
2022-12-19 12:41:19 -0500

    IIFE addition
    Wrapped herbList in an IIFE, created herbRepository. Added console log

--- herb-js-/js/scripts.js
+++ herb-js-/js/scripts.js
@@ -1,36 +1,51 @@
-let herbRepository                                                                                                           
-let herbList = [
-    {
-        name: 'lavender',
-        latin: 'Lavandula spp',
-        type: 'antibacterial',
-        color: 'purple',
-        height: 20
-    },
-    {
-        name: 'rosemary',
-        latin: 'Rosmarinus officinalis',
-        type: 'antimicrobial',
-        color: 'green',
-        height: 6
-    },
-
-    {
-        name:  'peppermint',
-        latin:  'Mentha x piperita',
-        type:  'antimicrobial' , 
-        color:  'green',
-        height:  18
+let herbRepository = (function() {
+    let herbList = [
+        {
+            name: 'lavender',
+            latin: 'Lavandula spp',
+            type: 'antibacterial',
+            color: 'purple',
+            height: 20
+        },
+        {
+            name: 'rosemary',
+            latin: 'Rosmarinus officinalis',
+            type: 'antimicrobial',
+            color: 'green',
+            height: 6
+        },
     
- },
-   {
-    name: 'chamomile',
-    latin: 'Matricaria chamomilla',
-    type: 'calmative',
-    color: 'white',
-    height: 3
-}
-];
+        {
+            name:  'peppermint',
+            latin:  'Mentha x piperita',
+            type:  'antimicrobial' , 
+            color:  'green',
+            height:  18
+        
+     },
+       {
+        name: 'chamomile',
+        latin: 'Matricaria chamomilla',
+        type: 'calmative',
+        color: 'white',
+        height: 3
+    }
+    ]
+    function getAll () {
+        return herbList;
+    }
+    function add (herb) {
+        herbList.push(herbs);
+    }
+
+    return{
+        getAll: getAll,
+        add: add
+    }
+})()                                                                                  
+
+
+console. log(herbRepository.getAll())
  //printing all herbs
 
     
